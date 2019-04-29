87cca20174b8507a7942c772e4871934b30bd629
Colin <Colin.F.Sullivan@colorado.edu>
2019-04-29 10:11:07 -0600

    Plot Function Integrated

--- ParseandPlot.m
+++ ParseandPlot.m
@@ -27,13 +27,61 @@ for i = 1:length(Channel2Nums)
         Channel2Nums(i) = Channel2Nums(i) + 2^16;
     end
 end
-figure; 
-plot(Channel1Nums(1:2:end));
+
+%Time Vector
+time1 = linspace(0,(1/250)*length(Channel1Nums),length(Channel1Nums));
+time2 = linspace(0,(1/250)*length(Channel1Nums),length(conv(Channel1Nums(1:2:end),ones(100,1)/100,'valid')));
+
+figure;
+subplot(2,2,1);
+plot(time1(1:2:end),Channel1Nums(1:2:end));
 hold on;
+grid on;
 title('Counts vs time')
-xlabel('Time')
+xlabel('Time [s]')
 ylabel('Counts');
-% figure
-% title('channel2')
-% plot(Channel2Nums)
-end
+
+subplot(2,2,2);
+plot(time2,conv(Channel1Nums(1:2:end),ones(100,1)/100,'valid'));
+hold on;
+grid on;
+title('Convoluted Counts v. Time');
+xlabel('Time [s]');
+ylabel('Counts');
+
+
+
+% Counts to Volts Conversion
+fid = fopen(filename);
+rangeid = str2num(fgetl(fid));
+fclose('all');
+
+Volts1 = Channel1Nums./65535;
+Volts1 = Volts1 * 10;
+
+% if ((rangeid & 1) ~= 0)
+%     Volts1 = Volts1 * 2 - 1;
+% end
+% if ((rangeid & 2) == 0)
+%     Volts1 = Volts1 * 2;
+% end
+% if ((rangeid & 4) == 0)
+%     Volts1 = Volts1 * 5;
+% end
+
+subplot(2,2,3);
+plot(time1(1:2:end),Volts1(1:2:end));
+hold on;
+grid on;
+title('Volts vs. time')
+xlabel('Time [s]')
+ylabel('Volts [V]');
+
+subplot(2,2,4);
+plot(time2,conv(Volts1(1:2:end),ones(100,1)/100,'valid'));
+hold on;
+title('Convoluted Volts v. Time');
+ylabel('Volts [V]');
+xlabel('Time [s]');
+
+
